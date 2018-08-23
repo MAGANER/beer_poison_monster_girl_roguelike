29f8f1101a4e51a6d9bf4e6d3cec7e582115fb20
maganer.krik <maganer.krik@mail.ru>
2018-08-23 15:46:18 +0200

    made start structure

---
+++ objects.h
@@ -0,0 +1,4 @@
+#pragma once
+#include"stdafx.h"
+
+RenderWindow window(sf::VideoMode(820, 600), "WORLDOFFUCKINGSORROW");
---
+++ rule_variables.h
@@ -0,0 +1,8 @@
+#pragma once
+// these variables are used to manage game
+
+
+bool hero_step = true;
+
+bool game_menu = true;
+bool game = false;
