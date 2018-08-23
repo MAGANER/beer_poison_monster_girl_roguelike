7f55309ebc79a15794d6ebd955ab1fd5a7b296d7
maganer.krik <maganer.krik@mail.ru>
2018-08-23 18:56:06 +0200

    made hero go

--- main.cpp
+++ main.cpp
@@ -7,15 +7,21 @@
 using namespace std;
 Text hero;
 
+void set_hero_on_map();
+void draw_action_line();
 void draw_vertical_borders();
 void draw_borders();
 int main()
 {
+	Vector2f hero_pos;
+
 	font.loadFromFile("9303.ttf");
 	hero.setFont(font);
-	hero.setCharacterSize(48);
+	hero.setCharacterSize(36);
 	hero.setString("@");
 
+
+	set_hero_on_map();
 	while (window.isOpen())
 	{
 //////////////////////////////////////////////
@@ -33,6 +39,87 @@ int main()
 
 /////////////////////////////////////////////
 ///////////ACTIONS///////////////////////////
+		if (hero_step)
+		{
+			if (Keyboard::isKeyPressed(Keyboard::W))
+			{
+
+				direction = "up";
+				
+			}
+			if (Keyboard::isKeyPressed(Keyboard::S))
+			{
+				direction = "down";
+				
+			}
+			if (Keyboard::isKeyPressed(Keyboard::A))
+			{
+				direction = "left";
+			}
+			if (Keyboard::isKeyPressed(Keyboard::D))
+			{
+				direction = "right";
+			}
+			if (!stop)
+			{
+				if (Keyboard::isKeyPressed(Keyboard::Space))
+				{
+					if (direction == "up")
+					{
+						Vector2f heropos = hero.getPosition();
+
+						hero_pos.x = heropos.x;
+						hero_pos.y = heropos.y;
+
+						hero_pos.y -= 32;
+						hero.setPosition(hero_pos);
+						stop = true;
+					}
+					if (direction == "down")
+					{
+						Vector2f heropos = hero.getPosition();
+
+						hero_pos.x = heropos.x;
+						hero_pos.y = heropos.y;
+
+						hero_pos.y += 32;
+						hero.setPosition(hero_pos);
+						stop = true;
+					}
+					if (direction == "left")
+					{
+						Vector2f heropos = hero.getPosition();
+
+						hero_pos.x = heropos.x;
+						hero_pos.y = heropos.y;
+
+						hero_pos.x -= 32;
+						hero.setPosition(hero_pos);
+						stop = true;
+					}
+					if (direction == "right")
+					{
+						Vector2f heropos = hero.getPosition();
+
+						hero_pos.x = heropos.x;
+						hero_pos.y = heropos.y;
+
+						hero_pos.x += 32;
+						hero.setPosition(hero_pos);
+						stop = true;
+					}
+				}
+			}
+			if (stop)
+			{	
+				if (!Keyboard::isKeyPressed(Keyboard::Space))
+				{
+						stop = false;
+				}
+	
+
+			}
+        }
 
 
 //////////////////////////////////////////////
@@ -42,6 +129,7 @@ int main()
 		window.clear();
 		window.draw(hero);
 		draw_vertical_borders();
+		draw_action_line();
 		draw_borders();
 		window.display();
 //////////////////////////////////////////////
@@ -103,7 +191,7 @@ void draw_vertical_borders()
 	border.setPosition(pos);
 	window.draw(border);
 
-	pos.x = 800;
+	pos.x = 810;
 	pos.y = 0;
 	for (int i = 0; i < 7; i++)
 	{
@@ -114,4 +202,35 @@ void draw_vertical_borders()
 	pos.y = 0;
 	border.setPosition(pos);
 	window.draw(border);
+}
+void draw_action_line()
+{
+	Text border;
+	border.setFont(font);
+	border.setString("fuckyou");
+	border.setCharacterSize(24);
+	Vector2f border_pos_up;
+	border_pos_up.x = 0;
+	border_pos_up.y = 140;
+	for (int i = 0; i < 7; i++)
+	{
+		window.draw(border);
+		border_pos_up.x += 120;
+		border.setPosition(border_pos_up);
+	}
+	border_pos_up.x = 0;
+	border_pos_up.y = 140;
+	border.setPosition(border_pos_up);
+	window.draw(border);
+}
+void set_hero_on_map()
+{
+	srand(time(0));
+	int y = 140 + rand() % 520;
+	int x = 20 + rand() % 680;
+
+	Vector2f hero_pos;
+	hero_pos.y =(float) y;
+	hero_pos.x = (float)x;
+	hero.setPosition(hero_pos);
 }
--- rule_variables.h
+++ rule_variables.h
@@ -1,6 +1,7 @@
 #pragma once
 // these variables are used to manage game
-
+#include"stdafx.h"
 
 bool hero_step = true;
-
+bool stop = false;
+string direction;
