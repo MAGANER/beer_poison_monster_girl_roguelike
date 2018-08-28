9abefd99a1095209ab4dbc021b18bd603783634d
maganer.krik <maganer.krik@mail.ru>
2018-08-28 16:10:46 +0200

    start refactoring again

--- Action.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include"stdafx.h"
-
-Vector2f action_pos;
-Text action;
--- objects.h
+++ objects.h
@@ -1,7 +1,25 @@
 #pragma once
 #include"stdafx.h"
 
+Font font;
 
+Vector2f hero_pos;
+Vector2f action_pos;
+Vector2f object_pos;
+RenderWindow window(sf::VideoMode(800, 600), "WORLDOFFUCKINGSORROW");
 
+Text action;
+Text hero;
+Text object;
 
-
+// show that at the begging
+Vector2f commandstg_pos;
+Text commands_to_go; 
+Vector2f commandste_pos;
+Text commands_to_eat;
+Vector2f commandstk_pos;
+Text commands_to_kill;
+Vector2f commandstr_pos;
+Text commands_to_return;
+Vector2f commandstt_pos;
+Text commands_to_take;
